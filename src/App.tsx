555c21076e838451ff40abeab887cc61303282cd
Necdet UYGUR <necdet.uygur@gmail.com>
2022-07-15 00:21:12 +0300

    Reload

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import FwAltin from "./components/FwAltin";
 import FwDoviz from "./components/FwDoviz";
 
 function App() {
+  document.addEventListener("click", function () {
+    if (window != null) {
+      // eslint-disable-next-line
+      window.location.href = window.location.href;
+    }
+  });
   return (
     <>
       <FwAltin />
