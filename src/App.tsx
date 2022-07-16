544e8deae05ffbd318133508bddb5474916db739
Necdet UYGUR <necdet.uygur@gmail.com>
2022-07-16 23:26:26 +0300

    Auto reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import Iko from "./components/Iko";
 import FwAltin from "./components/FwAltin";
 import FwDoviz from "./components/FwDoviz";
 
 function App() {
+  React.useEffect(() => {
+    setTimeout(() => {
+      // eslint-disable-next-line
+      window.location.href = window.location.href;
+    }, 90e3);
+    // eslint-disable-next-line
+  }, []);
   document.addEventListener("click", function () {
     if (window != null) {
       // eslint-disable-next-line
