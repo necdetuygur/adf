d797409b175878a2427a08ef37ed34d169f8e336
Necdet UYGUR <necdet.uygur@gmail.com>
2025-01-13 00:25:36 +0300

    Ready

---
+++ 573.1cdf7a66df1bb97d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkng_adf=self.webpackChunkng_adf||[]).push([[573],{573:(u,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>o});var _=e(885),s=e(201);class o extends s.E_{show(d){return(0,_.A)(function*(){})()}hide(d){return(0,_.A)(function*(){})()}}}}]);
